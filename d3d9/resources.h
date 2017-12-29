f314531a959e734d1b84d9881b2621c43460db83
saumyamukul <u0886819@utah.edu>
2017-12-29 13:51:21 -0700

    Updating

--- d3d9/resources.h
+++ d3d9/resources.h
@@ -7,67 +7,64 @@
 class Resources{
 public:
 	Resources(){
+		auto window_rect = _get_window_rect();
+		auto window_width = window_rect.right - window_rect.left;
+		auto window_height = window_rect.bottom - window_rect.top;
 
+		_image_width = window_width*_image_width_percent;
+		_image_height = window_height*_image_height_percent;
+
+		_dock_width = window_width*_dock_width_percent;
+		_dock_height = window_height*_dock_height_percent;
+
+		_arrow_width = window_width*_arrow_width_percent;
+		_arrow_height = window_height*_arrow_height_percent;
+
+		_text_width = window_width*_text_width_percent;
+		_text_height = window_width*_text_height_percent;
+
+		_dock_padding_top = (window_height - _dock_height) / 2.0f;
+		_image_padding_left = (_dock_width - _image_width) / 2.0f;
+		_image_padding_top = (_dock_height - _image_height) / 2.0f;
+		_text_padding_top = window_height*1.8f;
+
+		_dock_position = D3DXVECTOR2(window_width - _dock_width, _dock_padding_top);
+		_image_position = D3DXVECTOR2(_dock_position.x + _image_padding_left, _dock_position.y + _image_padding_top);
+		_arrow_center_position = D3DXVECTOR2(window_width - _dock_width / 2.0f, _image_position.y + _image_height / 2.0f - _arrow_height / 2.0f);
+		_text_center_position = D3DXVECTOR2(window_width - _dock_width / 2.0f, _image_position.y + _image_height + _text_padding_top);
 	}
 
 	std::vector<TextureInfo> get_hud_texture_info(){
 		std::vector<TextureInfo> texture_info = {
-			{ "dock_frame", D3DXVECTOR2(dock_width, dock_height) },
-			{ "left_arrow", D3DXVECTOR2(arrow_width, arrow_height) },
-			{ "right_arrow", D3DXVECTOR2(arrow_width, arrow_height) },
-			{ "default_image", D3DXVECTOR2(image_width, image_height) },
+			{ "dock_frame", D3DXVECTOR2(_dock_width, _dock_height) },
+			{ "left_arrow", D3DXVECTOR2(_arrow_width, _arrow_height) },
+			{ "right_arrow", D3DXVECTOR2(_arrow_width, _arrow_height) },
+			{ "default_image", D3DXVECTOR2(_image_width, _image_height) },
 		};
 		return texture_info;
 	}
 
 	std::vector<SpriteInfo> get_sprite_info(){
-		auto window_rect = _get_window_rect();
-		auto window_width = window_rect.right - window_rect.left;
-		auto window_height = window_rect.bottom - window_rect.top;
-
-		auto dock_padding_top = (window_height - dock_height) / 2.0f;
-		auto image_padding_left = (dock_width - image_width) / 2.0f;
-		auto image_padding_top = (dock_height - image_height) / 2.0f;
-		auto arrows_padding_top = 20.0f;
-
-		D3DXVECTOR2 dock_position = D3DXVECTOR2(window_width - dock_width, dock_padding_top);
-		D3DXVECTOR2 image_position = D3DXVECTOR2(dock_position.x + image_padding_left, dock_position.y + image_padding_top);
-		D3DXVECTOR2 arrow_center_position = D3DXVECTOR2(window_width - dock_width / 2.0f, image_position.y + image_height / 2.0f - arrow_height / 2.0f);
-
-		auto arrow_offsets_from_center = image_width / 2.0f + 10.0f;
-
-		D3DXVECTOR2 left_arrow_pos = arrow_center_position + D3DXVECTOR2(-arrow_offsets_from_center - arrow_width / 2.0f, 0.0f);
-		D3DXVECTOR2 right_arrow_pos = arrow_center_position + D3DXVECTOR2(arrow_offsets_from_center - arrow_width / 2.0f, 0.0f);
+		auto arrow_offset_from_center = _image_width / 1.9f;
+		D3DXVECTOR2 left_arrow_pos = _arrow_center_position + D3DXVECTOR2(-arrow_offset_from_center - _arrow_width / 2.0f, 0.0f);
+		D3DXVECTOR2 right_arrow_pos = _arrow_center_position + D3DXVECTOR2(arrow_offset_from_center - _arrow_width / 2.0f, 0.0f);
 
 		std::vector<SpriteInfo> sprite_info = {
-			{ "dock_frame", "dock_frame", dock_position, D3DXVECTOR2(dock_width, dock_height) },
-			{ "hero_image", "default_image", image_position, D3DXVECTOR2(image_width, image_height) },
-			{ "left_arrow", "left_arrow", left_arrow_pos, D3DXVECTOR2(arrow_width, arrow_height) },
-			{ "right_arrow", "right_arrow", right_arrow_pos, D3DXVECTOR2(arrow_width, arrow_height) },
+			{ "dock_frame", "dock_frame", _dock_position, D3DXVECTOR2(_dock_width, _dock_height) },
+			{ "hero_image", "default_image", _image_position, D3DXVECTOR2(_image_width, _image_height) },
+			{ "left_arrow", "left_arrow", left_arrow_pos, D3DXVECTOR2(_arrow_width, _arrow_height) },
+			{ "right_arrow", "right_arrow", right_arrow_pos, D3DXVECTOR2(_arrow_width, _arrow_height) },
 		};
 		return sprite_info;
 	}
 
 	std::vector<TextAsset> get_text_assets(){
-
-		auto window_rect = _get_window_rect();
-		auto window_width = window_rect.right - window_rect.left;
-		auto window_height = window_rect.bottom - window_rect.top;
-
-		auto dock_padding_top = (window_height - dock_height) / 2.0f;
-		auto image_padding_left = (dock_width - image_width) / 2.0f;
-		auto image_padding_top = (dock_height - image_height) / 2.0f;
-		auto arrows_padding_top = 30.0f;
-
-		D3DXVECTOR3 dock_position = D3DXVECTOR3(window_width - dock_width, dock_padding_top, 0);
-		D3DXVECTOR3 image_position = D3DXVECTOR3(dock_position.x + image_padding_left, dock_position.y + image_padding_top, 0);
-		D3DXVECTOR3 text_center_position = D3DXVECTOR3(window_width - dock_width / 2.0f, image_position.y + image_height + arrows_padding_top, 0);
 		RECT font_rect;
-		SetRect(&font_rect, text_center_position.x - 20.0f, text_center_position.y - 20.0f, text_center_position.x + 20.0f, text_center_position.x + 20.0f);
+		SetRect(&font_rect, _text_center_position.x - _text_width / 2.0f, _text_center_position.y - _text_height / 2.0f, _text_center_position.x + _text_width / 2.0f, _text_center_position.x + _text_height/2.0f);
 
 		return{ { "hero_edge", font_rect, 0xFFFFFFFF, "N/A" } };
 	}
-	D3DXVECTOR2 get_image_size(){ return{ image_width, image_height }; }
+	D3DXVECTOR2 get_image_size(){ return{ _image_width, _image_height }; }
 private:
 
 	RECT _get_window_rect(){
@@ -78,17 +75,36 @@ private:
 	}
 
 	// Image
-	const float image_width = 150.0f;
-	const  float image_height = 84.0f;
-	const float arrows_padding_top = 20.0f;
+	const float _image_width_percent = 0.077f;
+	const float _image_height_percent = 0.077f;
+	float _image_width;
+	float _image_height;
+	float _image_padding_top;
+	float _image_padding_left;
+	D3DXVECTOR2 _image_position;
 
 	// Arrow
-	const  float arrow_width = 16.0f;
-	const  float arrow_height = 16.0f;
+	const  float _arrow_width_percent = 0.008f;
+	const  float _arrow_height_percent = 0.008f;
+	float _arrow_width;
+	float _arrow_height;
+	D3DXVECTOR2 _arrow_center_position;
 
 	// Dock
-	const  float dock_width = 200.0f;
-	const  float dock_height = 1024.0f;
+	const  float _dock_width_percent = 0.095f;
+	const  float _dock_height_percent = 0.8f;
+	float _dock_width;
+	float _dock_height;
+	float _dock_padding_top;
+	D3DXVECTOR2 _dock_position;
+
+	// Text
+	const float _text_width_percent = 2.0f;
+	const float _text_height_percent = 2.0f;
+	float _text_width;
+	float _text_height;
+	float _text_padding_top;
+	D3DXVECTOR2 _text_center_position;
 };
 
 
